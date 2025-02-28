47c7ef3935d22834133e14b3a0eb8c9b1aea812d
Jalene Is'raell Armstrong <jalenearmstrong13@gmail.com>
2025-02-28 15:59:58 -0400

    Attempt: Deployment Error
    Testing the backend's deployment on render, the log states that (psycopg2.errors.StringDataRightTruncation) value too long for type character varying(120), this is in reference to the password length being 120, which does hold true as the password Render shows counted to be 162, so checking to see if this is the only issue

--- App/models/user.py
+++ App/models/user.py
@@ -6,7 +6,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), nullable=False, unique=False)
     email =  db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
 
     def __init__(self, name, email, password):
         self.name = name
