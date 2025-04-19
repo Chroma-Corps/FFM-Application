1f0d1f4868517ec979d5f20f2307ab3ce13401dc
Jalene Is'raell Armstrong <jalenearmstrong13@gmail.com>
2025-04-19 11:52:11 -0400

    Integration Test: Transaction

--- App/tests/test_app.py
+++ App/tests/test_app.py
@@ -1,6 +1,4 @@
-import datetime
 import pytest, logging, unittest
-from flask import current_app
 from App.main import create_app
 from App.database import db, create_db
 from App.models import *
@@ -972,6 +970,7 @@ class BankIntegrationTests(unittest.TestCase):
         deletedbank = get_bank(bankID=bank.bankID)
         self.assertIsNone(deletedbank)
 
+# Budget
 class BudgetIntegrationTests(unittest.TestCase):
 
     def test_int_16_create_budget(self):
@@ -1127,6 +1126,7 @@ class BudgetIntegrationTests(unittest.TestCase):
         deletedbudget = get_budget(budgetID=budget.budgetID)
         self.assertIsNone(deletedbudget)
 
+# Goal
 class GoalIntegrationTests(unittest.TestCase):
 
     def test_int_20_create_goal(self):
@@ -1239,124 +1239,216 @@ class GoalIntegrationTests(unittest.TestCase):
         deletedgoal = get_goal(goalID=goal.goalID)
         self.assertIsNone(deletedgoal)
 
-# class TransactionIntegrationTests(unittest.TestCase):
+# Transaction
+class TransactionIntegrationTests(unittest.TestCase):
 
-#     def test_int_09_add_transaction(self):
-#         newuser = create_user(name="Sunny Sunflower", email="sunny@mail.com", password="sunnypass")
-#         newbank = create_bank(userID=newuser.id, bankTitle="Sunny Bank", bankCurrency="TTD", bankAmount=5000.00)
-#         newbudget = create_budget(budgetTitle="Sunny Budget", budgetAmount=500.00, budgetType=BudgetType.EXPENSE, budgetCategory="TRANSIT", startDate="2025-01-01", endDate="2025-01-31", userID=newuser.id, bankID=newbank.bankID)
-#         newtransaction = add_transaction(
-#             userID=newuser.id, 
-#             transactionTitle="Ride To School", 
-#             transactionDesc="Usual bus route ride to school", 
-#             transactionType=TransactionType.EXPENSE, 
-#             transactionCategory="TRANSIT", 
-#             transactionAmount=7.00, 
-#             transactionDate="2025-01-06",
-#             transactionTime="09:30",
-#             budgetID=newbudget.budgetID
-#         )
-#         transaction = get_transaction(newtransaction.transactionID)
-#         assert transaction.transactionTitle == "Ride To School"
+    def test_int_24_add_transaction(self):
+        user = create_user(name="Sunny Sunflower",
+                           email="sunny@mail.com",
+                           password="sunnypass")
 
-#     def test_int_10_user_transactions_json(self):
-#         newuser = create_user(name="Sonni Bunni", email="sonni@mail.com", password="sonniepass")
-#         newbank = create_bank(userID=newuser.id, bankTitle="Sonni Bank", bankCurrency="USD", bankAmount=3700.00)
-#         newbudget = create_budget(budgetTitle="Sonni Budget", budgetAmount=760.00, budgetType=BudgetType.EXPENSE, budgetCategory="BILLS", startDate="2025-01-01", endDate="2025-01-31", userID=newuser.id, bankID=newbank.bankID)
-#         newtransaction = add_transaction(
-#             userID=newuser.id, 
-#             transactionTitle="Movie Night", 
-#             transactionDesc="Spontaneous Movie Night", 
-#             transactionType=TransactionType.EXPENSE, 
-#             transactionCategory="ENTERTAINMENT", 
-#             transactionAmount=30.00, 
-#             transactionDate="2025-02-10",
-#             transactionTime="17:30",
-#             budgetID=newbudget.budgetID,
-#             bankID=newbank.bankID
-#         )
-#         user_transaction_json = get_user_transactions_json(newuser.id)
-#         self.assertListEqual([{"userID":newuser.id, 
-#                                "transactionID":newtransaction.transactionID,
-#                                "transactionTitle":"Movie Night",
-#                                "transactionDescription":"Spontaneous Movie Night",
-#                                "transactionType":"Expense",
-#                                "transactionCategory":"ENTERTAINMENT",
-#                                "transactionAmount": "$30.00",
-#                                "transactionDate": "Mon, 10 Feb 2025",
-#                                "transactionTime": "17:30" },
-#                               ], user_transaction_json)
+        circle = create_circle(circleName="Sunny Circle",
+                        circleType=CircleType.SELF,
+                        circleColor="#6A3D9A",
+                        circleImage="https://picsum.photos/id/82/300/300.jpg",
+                        userID=user.id)
 
-#     def test_int_11_update_transaction(self):
-#         newuser = create_user(name="Larry Pinhead", email="larry@mail.com", password="larrypass")
-#         newbank = create_bank(userID=newuser.id, bankTitle="Larry Bank", bankCurrency="TTD", bankAmount=300.00)
-#         newbudget = create_budget(budgetTitle="Larry Budget", budgetAmount=10.00, budgetType=BudgetType.EXPENSE, budgetCategory="ENTERTAINMENT", startDate="2025-01-01", endDate="2025-01-31", userID=newuser.id, bankID=newbank.bankID)
-#         newtransaction = add_transaction(
-#             userID=newuser.id, 
-#             transactionTitle="Picnic Party", 
-#             transactionDesc="Had a small picnic with friends", 
-#             transactionType=TransactionType.EXPENSE, 
-#             transactionCategory="Bills", 
-#             transactionAmount=75.00, 
-#             transactionDate="2025-02-01",
-#             transactionTime="14:30",
-#             budgetID=newbudget.budgetID,
-#             bankID=newbank.bankID
-#         )
-#         update_transaction(id=newtransaction.transactionID, transactionTitle=None, transactionDesc=None, transactionType=None, transactionCategory="ENTERTAINMENT", transactionAmount=None, transactionDate=None, transactionTime=None, budgetID=None)
-#         transaction = get_transaction(newtransaction.transactionID)
-#         assert transaction.transactionCategory == "ENTERTAINMENT"
+        set_active_circle(userID=user.id, circleID=circle.circleID)
 
-#     def test_int_12_void_transaction(self):
-#         newuser = create_user(name="Laura Lynn", email="laura@mail.com", password="laurapass")
-#         newbank = create_bank(userID=newuser.id, bankTitle="Laura Bank", bankCurrency="USD", bankAmount=1500.00)
-#         newbudget = create_budget(budgetTitle="Larua Budget", budgetAmount=700.00, budgetType=BudgetType.SAVINGS, budgetCategory="INCOME", startDate="2025-01-01", endDate="2025-01-31", userID=newuser.id, bankID=newbank.bankID)
-#         newtransaction = add_transaction(
-#             userID=newuser.id, 
-#             transactionTitle="Savings Budget", 
-#             transactionDesc="A Savings Budget", 
-#             transactionType=TransactionType.INCOME, 
-#             transactionCategory="Income", 
-#             transactionAmount=100.00, 
-#             transactionDate="2025-02-01",
-#             transactionTime="14:30",
-#             budgetID=newbudget.budgetID
-#         )
-#         void_transaction(id=newtransaction.transactionID)
-#         transaction = get_transaction(newtransaction.transactionID)
-#         assert transaction.voided == True
+        bank = create_bank(userID=user.id,
+                           bankTitle="Sunny Wallet",
+                           bankCurrency="TTD",
+                           bankAmount=1000,
+                           isPrimary=True,
+                           color="#6A3D9A")
 
-# class BankIntegrationTests(unittest.TestCase):
+        budget = create_budget(budgetTitle="Sunny Budget", 
+                                budgetAmount=200.00,
+                                budgetType=BudgetType.EXPENSE,
+                                budgetCategory=["TRANSIT"],
+                                transactionScope=TransactionScope.INCLUSIVE,
+                                color="#6A3D9A",
+                                startDate="2025-01-01",
+                                endDate="2025-01-31",
+                                userID=user.id,
+                                bankID=bank.bankID)
 
-#     def test_int_13_create_bank(self):
-#         newuser = create_user(name="Curt Curtis", email="curt@mail.com", password="curtpass")
-#         newbank = create_bank(userID=newuser.id, bankTitle="Curt Bank", bankCurrency="TTD", bankAmount=50000.00)
-#         bank = get_bank(newbank.bankID)
-#         assert bank.bankTitle == "Curt Bank"
-#         assert bank.bankAmount == 50000.00
+        add_transaction(
+            transactionTitle="Ride To School", 
+            transactionDesc="Usual bus route ride to school", 
+            transactionType=TransactionType.EXPENSE, 
+            transactionCategory=["TRANSIT"], 
+            transactionAmount=10.00, 
+            transactionDate="2025-01-06",
+            transactionTime="09:30",
+            bankID=bank.bankID,
+            userID=user.id,
+            goalID=None,
+            budgetID=budget.budgetID
+        )
 
-#     def test_int_14_get_user_banks(self):
-#         newuser = create_user(name="James Klug", email="klug@mail.com", password="klugpass")
-#         newbank = create_bank(userID=newuser.id, bankTitle="Klug Bank", bankCurrency="USD", bankAmount=2300.00)
-#         user_banks_json = get_user_banks_json(newuser.id)
-#         self.assertListEqual([{"bankID":newbank.bankID,
-#                                "userID":newuser.id, 
-#                                "bankTitle":"Klug Bank",
-#                                "bankCurrency":"USD",
-#                                "bankAmount":"$2300.00",
-#                                "remainingBankAmount": "$2300.00"},
-#                               ], user_banks_json)
+        assert bank.remainingBankAmount == 990
 
-#     def test_int_15_update_bank(self):
-#         newuser = create_user(name="Misha Petrov", email="misha@mail.com", password="mishapass")
-#         newbank = create_bank(userID=newuser.id, bankTitle="Misha Bank", bankCurrency="TTD", bankAmount=200.00)
-#         update_bank(bankID=newbank.bankID, bankTitle="Petrov Bank", bankCurrency=None, bankAmount=None)
-#         bank = get_bank(newbank.bankID)
-#         assert bank.bankTitle == "Petrov Bank"
+    def test_int_25_user_transactions_json(self):
+        user = create_user(name="Sonni Bunni",
+                           email="sonni@mail.com",
+                           password="sonnipass")
 
-#     def test_int_16_delete_bank(self):
-#         newuser = create_user(name="Hannah Alonzo", email="hannah@mail.com", password="hannahpass")
-#         newbank = create_bank(userID=newuser.id, bankTitle="Hannah Bank", bankCurrency="TTD", bankAmount=10000.00)
-#         delete_bank(newbank.bankID)
-#         deletedbank = get_budget(newbank.bankID)
-#         assert deletedbank is None
+        circle = create_circle(circleName="Sonni Circle",
+                        circleType=CircleType.SELF,
+                        circleColor="#6A3D9A",
+                        circleImage="https://picsum.photos/id/82/300/300.jpg",
+                        userID=user.id)
+
+        set_active_circle(userID=user.id, circleID=circle.circleID)
+
+        bank = create_bank(userID=user.id,
+                           bankTitle="Sonni Wallet",
+                           bankCurrency="USD",
+                           bankAmount=3700,
+                           isPrimary=True,
+                           color="#6A3D9A")
+
+        budget = create_budget(budgetTitle="Sonni Budget", 
+                                budgetAmount=760.00,
+                                budgetType=BudgetType.EXPENSE,
+                                budgetCategory=["ENTERTAINMENT"],
+                                transactionScope=TransactionScope.INCLUSIVE,
+                                color="#6A3D9A",
+                                startDate="2025-01-01",
+                                endDate="2025-01-31",
+                                userID=user.id,
+                                bankID=bank.bankID)
+
+        newtransaction, _ = add_transaction(
+            transactionTitle="Movie Night", 
+            transactionDesc="Spontaneous Movie Night", 
+            transactionType=TransactionType.EXPENSE, 
+            transactionCategory=["ENTERTAINMENT", "TRANSIT"], 
+            transactionAmount=30.00, 
+            transactionDate="2025-02-10",
+            transactionTime="17:30",
+            bankID=bank.bankID,
+            userID=user.id,
+            goalID=None,
+            budgetID=budget.budgetID
+        )
+
+        user_transactions_json = get_user_transactions_json(userID=user.id)
+        expected = [{
+                "transactionID": newtransaction.transactionID,
+                "transactionTitle":"Movie Night",
+                "transactionDescription":"Spontaneous Movie Night",
+                "transactionAmount": "$30.00",
+                "transactionType":"Expense",
+                "transactionCategory": ["Entertainment", "Transit"],
+                "transactionDate": "Mon, Feb 10 2025",
+                "transactionTime": "17:30",
+                "transactionBank": bank.bankID,
+                "transactionBudget":budget.budgetID,
+                "transactionGoal": None,
+                "attachments": [],
+                "owner": "Sonni Bunni"
+                }]
+
+        self.assertListEqual(user_transactions_json, expected)
+    
+    def test_int_26_update_transaction(self):
+        user = create_user(name="Larry Pinhead",
+                    email="larry@mail.com",
+                    password="larrypass")
+
+        circle = create_circle(circleName="Larry Circle",
+                        circleType=CircleType.SELF,
+                        circleColor="#6A3D9A",
+                        circleImage="https://picsum.photos/id/82/300/300.jpg",
+                        userID=user.id)
+
+        set_active_circle(userID=user.id, circleID=circle.circleID)
+
+        bank = create_bank(userID=user.id,
+                           bankTitle="Larry Wallet",
+                           bankCurrency="TTD",
+                           bankAmount=300,
+                           isPrimary=True,
+                           color="#6A3D9A")
+
+        budget = create_budget(budgetTitle="Larry Budget", 
+                                budgetAmount=1000.00,
+                                budgetType=BudgetType.EXPENSE,
+                                budgetCategory=["ENTERTAINMENT"],
+                                transactionScope=TransactionScope.INCLUSIVE,
+                                color="#6A3D9A",
+                                startDate="2025-01-01",
+                                endDate="2025-01-31",
+                                userID=user.id,
+                                bankID=bank.bankID)
+
+        newtransaction = add_transaction(
+            transactionTitle="Picanic Party", 
+            transactionDesc="Had a small picnic with friends", 
+            transactionType=TransactionType.EXPENSE, 
+            transactionCategory=["ENTERTAINMENT"], 
+            transactionAmount=75.00, 
+            transactionDate="2025-01-06",
+            transactionTime="14:30",
+            bankID=bank.bankID,
+            userID=user.id,
+            goalID=None,
+            budgetID=budget.budgetID,
+        )
+
+        transaction = newtransaction[0]
+
+        update_transaction(transactionID=transaction.transactionID, transactionTitle="Picnic Party")
+        updated_transaction = get_transaction(transaction.transactionID)
+        assert updated_transaction.transactionTitle == "Picnic Party"
+
+    def test_int_27_void_transaction(self):
+        user = create_user(name="Laura Lynn",
+                    email="laura@mail.com",
+                    password="laurapass")
+
+        circle = create_circle(circleName="Larua Circle",
+                        circleType=CircleType.SELF,
+                        circleColor="#6A3D9A",
+                        circleImage="https://picsum.photos/id/82/300/300.jpg",
+                        userID=user.id)
+
+        set_active_circle(userID=user.id, circleID=circle.circleID)
+
+        bank = create_bank(userID=user.id,
+                           bankTitle="Larua Wallet",
+                           bankCurrency="USD",
+                           bankAmount=1500,
+                           isPrimary=True,
+                           color="#6A3D9A")
+
+        budget = create_budget(budgetTitle="Larua Budget", 
+                                budgetAmount=700.00,
+                                budgetType=BudgetType.SAVINGS,
+                                budgetCategory=["INCOME"],
+                                transactionScope=TransactionScope.INCLUSIVE,
+                                color="#6A3D9A",
+                                startDate="2025-01-01",
+                                endDate="2025-01-31",
+                                userID=user.id,
+                                bankID=bank.bankID)
+
+        newtransaction, _ = add_transaction(
+            transactionTitle="Savings", 
+            transactionDesc="", 
+            transactionType=TransactionType.INCOME, 
+            transactionCategory=["INCOME"], 
+            transactionAmount=100.00, 
+            transactionDate="2025-02-01",
+            transactionTime="14:30",
+            bankID=bank.bankID,
+            userID=user.id,
+            goalID=None,
+            budgetID=budget.budgetID
+        )
+
+        void_transaction(userID=user.id, transactionID=newtransaction.transactionID)
+        transaction = get_transaction(newtransaction.transactionID)
+        assert transaction.voided == True
